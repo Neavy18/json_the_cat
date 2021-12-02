aee1641b554d1056fc3145a77929a38d5b3b247d
Neavy18 <genevieve.gt18@gmail.com>
2021-12-02 18:59:12 -0500

    Add test unit and index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
 const request = require("request");
-const breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response,body) => {
-  if (error) {
-    console.log('error :', error);
-  }
-  console.log('status code:', response.statusCode);
 
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Sorry, we don't have info about that breed!");
-  } else {
-    console.log("this is the dataaaaa -->", data[0].description);
-  }
+const fetchBreedDescription = function(breed, callback) {
 
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response,body) => {
 
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback("Sorry, we don't have info about that breed!");
+    } else {
+      callback(error, data[0].description);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
